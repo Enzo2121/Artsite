78c0e464a25e01839fe269bd9ee1ec14605e950c
Enzo212 <Alienz1@outlook.fr>
2025-01-15 16:10:15 +0100

    mod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@nuxthq/studio'
+  ],
   routeRules: {
     '/': { prerender: true }
   }
